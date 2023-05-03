fa7fc1ca5ef72a7df2664c1c6b1aaedf872ddc2e
Jon Crain <jon.crain@unity3d.com>
2023-05-03 17:10:36 -0400

    :crossed_fingers:

--- autopkg_tools.py
+++ autopkg_tools.py
@@ -205,6 +205,8 @@ def handle_recipe(recipe, opts):
         AUTOPKG_REPO.git.worktree("add", branch_name, "-b", branch_name)
         autopkg_worktree_path = os.path.join(AUTOPKG_REPO_DIR, branch_name)
         autopkg_worktree_repo = git.Repo(autopkg_worktree_path)
+        print(recipe.path)
+        print(autopkg_worktree_path)
         autopkg_worktree_repo.git.add(os.path.join("overrides", recipe.path))
         autopkg_worktree_repo.git.commit(m=f"Update trust for {recipe.name}")
         autopkg_worktree_repo.git.push("--set-upstream", "origin", branch_name)
